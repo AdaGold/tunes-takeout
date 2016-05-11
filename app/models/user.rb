ff8a0b7f1a3361c218b2e9b8722b895af6520312
Charles Ellis <hamled@hamled.name>
2016-05-10 21:57:52 -0700

    User model is wrapper around Spotify User
    We use the RSpotify gem's User class to hold the actual user details
    from Spotify. This data is stored in the DB with the User model, as a
    hash in JSON format, and updated whenever new data is received by the
    OAuth authentication process.
    
    The User model then is just a simple wrapper around the RSpotify::User
    class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  def spotify_data
+    # Initialize a Spotify User from previously persisted hash data
+    @spotify_user ||= RSpotify::User.new(JSON.parse(super))
+  end
+
+  private
+
+  def spotify_data=(hash)
+    # Create a new Spotify User and persist its hash data (as JSON)
+    @spotify_user = RSpotify::User.new(hash)
+    write_attribute(:spotify_data, @spotify_user.to_hash.to_json)
+
+    return @spotify_user
+  end
 end
