5b60f5a8013ccb4e0e3e13101b1c91dcda2f704e
Charles Ellis <hamled@hamled.name>
2016-05-11 11:06:43 -0700

    SpotifyItem builds from API data

--- app/models/spotify_item.rb
+++ app/models/spotify_item.rb
@@ -1,2 +1,20 @@
 class SpotifyItem < ActiveRecord::Base
+
+  # Automatically build a new instance from attributes on an API object
+  include BuildsFromAPI
+  ATTRS_FROM_API = %w(item_id type name url image_url)
+  ATTR_MAP = {
+    item_id: :id,
+    url: ->(item) {
+      item.external_urls["spotify"]
+    },
+    image_url: ->(item) {
+      case item.type
+      when "album" || "artist" || "playlist"
+        item.images[0]["url"]
+      when "track"
+        item.album.images[0]["url"]
+      end
+    }
+  }
 end
