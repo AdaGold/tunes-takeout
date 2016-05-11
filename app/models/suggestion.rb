c30a7d5c2fab5285e2a964ff702547a1d94bae0f
Charles Ellis <hamled@hamled.name>
2016-05-11 11:07:15 -0700

    Suggestion builds from API data

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,4 +1,11 @@
 class Suggestion < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :spotify_item
+
+  def self.new_from_api(business, spotify_item)
+    return Suggestion.new do |suggestion|
+      suggestion.restaurant = Restaurant.new_from_api(business)
+      suggestion.spotify_item = SpotifyItem.new_from_api(item)
+    end
+  end
 end
