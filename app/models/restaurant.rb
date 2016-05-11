b7d3d260611bf35fab7cf48de98c215bbe335c84
Charles Ellis <hamled@hamled.name>
2016-05-11 11:06:30 -0700

    Restaurant builds from API data

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+
+  # Automatically build a new instance from attributes on an API object
+  include BuildsFromAPI
+  ATTRS_FROM_API = %w(business_id name url image_url phone rating)
+  ATTR_MAP = {business_id: :id, phone: :display_phone}
 end
