8b7fbe0fb47a286263477279a8fce13fdb0ecf43
Charles Ellis <hamled@hamled.name>
2016-05-12 16:13:08 -0700

    Require sign-in to view suggestions

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,6 +4,10 @@ class SuggestionsController < ApplicationController
   DEFAULT_SUGGESTION_COUNT = 10
 
   def index
+    if !signed_in?
+      return redirect_to login_path
+    end
+
     @suggestions = Suggestion.find_random(params[:q] || DEFAULT_SUGGESTION_SEARCH,
                                           DEFAULT_SUGGESTION_COUNT)
   end
