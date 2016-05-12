f7fe8f237dfadca73c832fdc16d7b128cb52448e
Charles Ellis <hamled@hamled.name>
2016-05-12 16:12:48 -0700

    Display random suggestions on home page

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,10 @@
 class SuggestionsController < ApplicationController
+
+  DEFAULT_SUGGESTION_SEARCH = "Ada"
+  DEFAULT_SUGGESTION_COUNT = 10
+
+  def index
+    @suggestions = Suggestion.find_random(params[:q] || DEFAULT_SUGGESTION_SEARCH,
+                                          DEFAULT_SUGGESTION_COUNT)
+  end
 end
