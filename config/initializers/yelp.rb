0bd1933aa29f8d939b204920df2253c557afd7cb
Charles Ellis <hamled@hamled.name>
2016-05-12 11:44:30 -0700

    Fix Yelp API configuration

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -2,7 +2,7 @@ require 'yelp'
 
 Yelp.client.configure do |config|
   config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_KEY']
+  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
   config.token = ENV['YELP_TOKEN_KEY']
-  config.token_secret = ENV['YELP_TOKEN_KEY']
+  config.token_secret = ENV['YELP_TOKEN_SECRET']
 end
