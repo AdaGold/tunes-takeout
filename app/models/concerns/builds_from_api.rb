b3ff7633cc2043fb575b207d9c5018bba9b32dcf
Charles Ellis <hamled@hamled.name>
2016-05-12 15:18:28 -0700

    Minor refactor for BuildsFromAPI concern

--- app/models/concerns/builds_from_api.rb
+++ app/models/concerns/builds_from_api.rb
@@ -2,17 +2,24 @@ module BuildsFromAPI
   extend ActiveSupport::Concern
 
   class_methods do
+
     def new_from_api(api_data)
       return self.new do |obj|
-        attr_map = Hash.new { |hash, key| key }.merge(self::API_ATTR_MAP)
-
         self::API_ATTR_LIST.map(&:to_sym).each do |attr|
-          mapped = attr_map[attr]
-          value = mapped.class == Proc ? mapped.call(api_data) : api_data.send(mapped)
-
-          obj.send("#{attr}=".to_sym, value)
+          obj.send("#{attr}=".to_sym, api_attr_get(api_data, attr))
         end
       end
+    end
+
+
+    # Utility methods
+    def api_attr_map
+      @attr_map ||= Hash.new { |hash, key| key }.merge(self::API_ATTR_MAP)
+    end
+
+    def api_attr_get(data, attr)
+      mapped = api_attr_map[attr]
+      return mapped.class == Proc ? mapped.call(data) : data.send(mapped)
     end
   end
 end
