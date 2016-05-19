28af326b0538d6a4f52f55b7eaeb576d41f8155f
melissajimison <melissajimison@gmail.com>
2016-05-19 16:33:16 -0700

    crated method for the top 20

--- lib/charles/TunesTakeoutWrapper.rb
+++ lib/charles/TunesTakeoutWrapper.rb
@@ -2,18 +2,30 @@ require 'httparty'
 module Charles
   class TunesTakeoutWrapper
     BASE_URL = "https://tunes-takeout-api.herokuapp.com"
+    FAVS_URL = "https://tunes-takeout-api.herokuapp.com/v1/suggestions/top"
     attr_reader :suggestions
     def initialize(data)
-      @suggestions = data["suggestions"]
+      @suggestions = data
+
       #@food_id = data["suggestions"]["food_id"]
       # @music_id = data["suggestions"]["music_id"]
       # @music_type = data["suggestions"]["music_type"]
+
+
     end
 
     def self.find(user_input)
       #find the suggestions base on the user_input
       data = HTTParty.get(BASE_URL + "/v1/suggestions/search?query=#{user_input}/").parsed_response
-      return self.new(data)
+      return self.new(data["suggestions"])
+    end
+
+    def self.all_favorites
+      data = HTTParty.get('https://tunes-takeout-api.herokuapp.com/v1/suggestions/top').parsed_response
+      total = data["suggestions"].map do |one_charles_id|
+      HTTParty.get(BASE_URL + "/v1/suggestions/#{one_charles_id}").parsed_response
+      end
+      return self.new(total)
     end
 
 
