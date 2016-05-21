05a92be70ccfb703afeb897ea8346f471f7541c8
melissajimison <melissajimison@gmail.com>
2016-05-21 16:09:17 -0700

    captured the open in Spotify from API

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,23 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_omniauth(auth_hash)
     # Find or create a user
+
     user = self.find_by(uid: auth_hash["info"]["id"], provider: auth_hash["provider"])
     if !user.nil?
       return user
     else
       user = User.new
-      user.uid = auth_hash["info"]["id"]
-      user.provider = auth_hash["provider"]
-      user.name = auth_hash["info"]["display_name"]
       if auth_hash["info"]["images"] == []
-      user.images = "http://i1.wp.com/www.techrepublic.com/bundles/techrepubliccore/images/icons/standard/icon-user-default.png"
+        user.uid = auth_hash["info"]["id"]
+        user.images = "http://i1.wp.com/www.techrepublic.com/bundles/techrepubliccore/images/icons/standard/icon-user-default.png"
       else
-      user.images = auth_hash["info"]["images"][0]["url"]
-    end
+        user.uid = auth_hash["uid"]
+        user.name = auth_hash["info"]["display_name"]
+        user.images = auth_hash["info"]["images"][0]["url"]
+      end
+      user.open_spotify_url = auth_hash["info"]["external_urls"]["spotify"]
+      user.provider = auth_hash["provider"]
+
       if user.save
         return user
       else
