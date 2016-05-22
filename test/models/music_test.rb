76009c3603b59e3f274be2e348cb1d459487eab6
melissajimison <melissajimison@gmail.com>
2016-05-22 00:27:49 -0700

    a 100% coverage user test

--- test/models/music_test.rb
+++ test/models/music_test.rb
@@ -1,7 +1,72 @@
 require 'test_helper'
 
 class MusicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # describe Music do
+  #   it "uses v2 of the API" do
+  #  assert_equal "http://pokeapi.co/api/v2/", Music::BASE_URL
   # end
+
+  describe "artist" do
+   before do
+     @intance_of_music = Music.create("artist","1jn4AT2jGKmlUVZWmuzNP4")
+   end
+   it "knows its name", :vcr do
+     assert_equal "Ricky Taco", @intance_of_music.name
+   end
+
+   it "knows its type", :vcr do
+    assert_equal "artist", @intance_of_music.type
+    end
+
+   it "knows that it has an image url", :vcr do
+     assert_instance_of String, @intance_of_music.image
+   end
+
+   it "knows that it has an open in spotify url", :vcr do
+     assert_instance_of String, @intance_of_music.open_spotify_url
+   end
+ end
+
+   describe "album" do
+    before do
+      @intance_of_music = Music.create("album","1Frb2yxLXQSaMzqnfCB1Dx")
+    end
+    it "knows its name", :vcr do
+      assert_equal "In the Style of Shakira - Vol. 2 (Karaoke - Professional Performance Tracks)", @intance_of_music.name
+    end
+
+    it "knows its type", :vcr do
+     assert_equal "album", @intance_of_music.type
+     end
+
+    it "knows that it has an image url", :vcr do
+      assert_instance_of String, @intance_of_music.image
+    end
+
+    it "knows that it has an open in spotify url", :vcr do
+      assert_instance_of String, @intance_of_music.open_spotify_url
+    end
+  end
+
+  describe "track" do
+   before do
+     @intance_of_music = Music.create("track","0FC7X3BRdEJC8L4ZEUAv2Q")
+   end
+   it "knows its name", :vcr do
+     assert_equal "Born This Way", @intance_of_music.name
+   end
+
+   it "knows its type", :vcr do
+    assert_equal "track", @intance_of_music.type
+    end
+
+   it "knows that it has an image url", :vcr do
+     assert_instance_of String, @intance_of_music.image
+   end
+
+   it "knows that it has an open in spotify url", :vcr do
+     assert_instance_of String, @intance_of_music.open_spotify_url
+   end
+ end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,20 @@ end
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
+require "minispec-metadata"
+require 'vcr'
+require 'minitest-vcr'
+require 'webmock/minitest'
+require "minitest/reporters"
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/cassettes'
+  c.hook_into :webmock
+end
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+MinitestVcr::Spec.configure!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
